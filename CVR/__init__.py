8c666b4a74148823701a05e77be44a97d2b43a80
ktroyan <klim.troyan@gmail.com>
2025-03-14 15:17:14 +0100

    refactor CVR folder with /external folder

---
+++ CVR/__init__.py
@@ -0,0 +1,8 @@
+from CVR.generate_cvr_data import main as generate_cvr_data
+from CVR.generate_cvr_data import ALL_TASKS_ID_TO_NAME, ALL_TASKS_NAME_TO_ID
+
+__all__ = [
+    'generate_cvr_data',
+    'ALL_TASKS_ID_TO_NAME',
+    'ALL_TASKS_NAME_TO_ID',
+    ]
