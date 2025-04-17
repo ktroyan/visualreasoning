eb7f80f2ea378af101ace7aedf08dc958bb89807
ktroyan <klim.troyan@gmail.com>
2025-04-18 01:22:16 +0200

    fix CLI handling for nohup job script

--- jobs/run_experiment_nohup.sh
+++ jobs/run_experiment_nohup.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
-# Default values for variables
-GPU_ID=1
-DATA_ENV="BEFOREARC"
-STUDY="sample-efficiency"
-SETTING="exp_setting_1"
-EXPERIMENT="experiment_1"
-MAX_EPOCHS=10
-BACKBONE="vit"
-HEAD="mlp"
-WANDB_SWEEP_ENABLED=false
+# Initialize variables as empty string (i.e., no defaults)
+GPU_ID=""
+DATA_ENV=""
+STUDY=""
+SETTING=""
+EXPERIMENT=""
+MAX_EPOCHS=""
+BACKBONE=""
+HEAD=""
+WANDB_SWEEP_ENABLED=""
 ADDI_LOG_NAME=""
 
-# Parse command line arguments
+# Parse CLI arguments
 while [[ $# -gt 0 ]]; do
     case "$1" in
         --gpu_id) GPU_ID="$2"; shift 2 ;;
@@ -32,29 +32,32 @@ while [[ $# -gt 0 ]]; do
     esac
 done
 
-# Set CUDA device
-export CUDA_VISIBLE_DEVICES=${GPU_ID}
+# Set CUDA device only if provided
+if [[ -n "$GPU_ID" ]]; then
+    export CUDA_VISIBLE_DEVICES=${GPU_ID}
+fi
 
 # Construct log filename
 LOG_FILE="output_${DATA_ENV}_${STUDY}_${SETTING}_${EXPERIMENT}_${BACKBONE}_${HEAD}${ADDI_LOG_NAME}.log"
 
-# Run the command in the background using nohup
-nohup uv run experiment.py \
-    base.gpu_id="${GPU_ID}" \
-    base.data_env="${DATA_ENV}" \
-    experiment.study="${STUDY}" \
-    experiment.setting="${SETTING}" \
-    experiment.name="${EXPERIMENT}" \
-    training.max_epochs="${MAX_EPOCHS}" \
-    model.backbone="${BACKBONE}" \
-    model.head="${HEAD}" \
-    wandb.sweep.enabled="${WANDB_SWEEP_ENABLED}" \
-    > "${LOG_FILE}" 2>&1 &
+# Construct command
+CMD="nohup uv run experiment.py"
 
-# Report success
+[[ -n "$GPU_ID" ]] && CMD+=" base.gpu_id=\"${GPU_ID}\""
+[[ -n "$DATA_ENV" ]] && CMD+=" base.data_env=\"${DATA_ENV}\""
+[[ -n "$STUDY" ]] && CMD+=" experiment.study=\"${STUDY}\""
+[[ -n "$SETTING" ]] && CMD+=" experiment.setting=\"${SETTING}\""
+[[ -n "$EXPERIMENT" ]] && CMD+=" experiment.name=\"${EXPERIMENT}\""
+[[ -n "$MAX_EPOCHS" ]] && CMD+=" training.max_epochs=\"${MAX_EPOCHS}\""
+[[ -n "$BACKBONE" ]] && CMD+=" model.backbone=\"${BACKBONE}\""
+[[ -n "$HEAD" ]] && CMD+=" model.head=\"${HEAD}\""
+[[ -n "$WANDB_SWEEP_ENABLED" ]] && CMD+=" wandb.sweep.enabled=\"${WANDB_SWEEP_ENABLED}\""
+
+CMD+=" > \"${LOG_FILE}\" 2>&1 &"
+
+# Run command
+eval $CMD
+
+# Echo messages
 echo "Launched experiment with PID $!"
 echo "Logs are being written to: ${LOG_FILE}"
-
-## Run the bash script
-# For example:
-# bash ./jobs/run_experiment_nohup.sh --data_env "BEFOREARC" --study "sample-efficiency" --setting "exp_setting_1" --experiment "experiment_16" --max_epochs 10 --backbone "vit" --head "mlp" --sweep_enabled false --gpu_id 0 --add_log_name ""
