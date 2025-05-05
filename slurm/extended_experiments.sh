b9b6c9f41fea324ed8ca93fc38ac2f882c0c4999
Pascal Sager <paes.sager@gmail.com>
2025-05-05 20:44:40 +0200

    Update runs

--- slurm/extended_experiments.sh
+++ slurm/extended_experiments.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
 # Choose which environments to run: options are "BEFOREARC", "REARC", or "BOTH"
-run_env="REARC"
+run_env="BOTH"
 
 # Define common parameters
-task_embedding_options=("false" "true")
 wandb_entity="sagerpascal"
 
 # Submit jobs
@@ -15,22 +14,36 @@ if [[ "$run_env" == "BEFOREARC" || "$run_env" == "BOTH" ]]; then
   sweep_types=("comp" "sysgen")
   experiment_settings=("es1" "es2" "es3" "es4" "es5")
 
-  for task_embedding in "${task_embedding_options[@]}"; do
-    for sweep_type in "${sweep_types[@]}"; do
-      for setting in "${experiment_settings[@]}"; do
-        config="base.data_env=${data_env} \
+  for sweep_type in "${sweep_types[@]}"; do
+    for setting in "${experiment_settings[@]}"; do
+      # Only enable task embedding for compositionality in BEFOREARC
+      if [[ "$sweep_type" == "comp" ]]; then
+        task_embedding="true"
+      else
+        task_embedding="false"
+      fi
+
+      # Set data flags explicitly
+      if [[ "$sweep_type" == "comp" || "$sweep_type" == "sysgen" ]]; then
+        use_gen_test_set="data.use_gen_test_set=true"
+        validate_in_and_out_domain="data.validate_in_and_out_domain=true"
+      else
+        use_gen_test_set="data.use_gen_test_set=false"
+        validate_in_and_out_domain="data.validate_in_and_out_domain=false"
+      fi
+
+      config="base.data_env=${data_env} \
 model.task_embedding.enabled=${task_embedding} \
 wandb.sweep.config=configs/sweeps/${data_env_lc}/${sweep_type}_${setting}.yaml \
 wandb.wandb_project_name=${wandb_project} \
 wandb.wandb_entity_name=${wandb_entity}"
-        echo "Submitting BEFOREARC: $config"
-        sbatch run_experiment.submit \
-          data.use_gen_test_set=true \
-          data.validate_in_and_out_domain=true \
-          wandb.sweep.enabled=true \
-          $config
-        sleep 61
-      done
+      echo "Submitting BEFOREARC: $config"
+      sbatch run_experiment.submit \
+        $use_gen_test_set \
+        $validate_in_and_out_domain \
+         wandb.sweep.enabled=true \
+        $config
+      sleep 61
     done
   done
 fi
@@ -42,32 +55,35 @@ if [[ "$run_env" == "REARC" || "$run_env" == "BOTH" ]]; then
   sweep_types=("se" "sysgen")
   experiment_settings=("es1")
 
-  for task_embedding in "${task_embedding_options[@]}"; do
-    for sweep_type in "${sweep_types[@]}"; do
-      for setting in "${experiment_settings[@]}"; do
-        use_gen_test_set="true"
-        validate_in_and_out_domain="true"
-        if [[ "$sweep_type" == "se" ]]; then
-          use_gen_test_set="false"
-          validate_in_and_out_domain="false"
-        fi
+  for sweep_type in "${sweep_types[@]}"; do
+    for setting in "${experiment_settings[@]}"; do
+      # Always disable task embedding for REARC
+      task_embedding="false"
 
-        config="base.data_env=${data_env} \
+      # Set data flags explicitly
+      if [[ "$sweep_type" == "comp" || "$sweep_type" == "sysgen" ]]; then
+        use_gen_test_set="data.use_gen_test_set=true"
+        validate_in_and_out_domain="data.validate_in_and_out_domain=true"
+      else
+        use_gen_test_set="data.use_gen_test_set=false"
+        validate_in_and_out_domain="data.validate_in_and_out_domain=false"
+      fi
+
+      config="base.data_env=${data_env} \
 model.task_embedding.enabled=${task_embedding} \
 wandb.sweep.config=configs/sweeps/${data_env_lc}/${sweep_type}_${setting}.yaml \
 wandb.wandb_project_name=${wandb_project} \
 wandb.wandb_entity_name=${wandb_entity}"
-        echo "Submitting REARC ($sweep_type): $config"
-        sbatch run_experiment.submit \
-          data.use_gen_test_set=$use_gen_test_set \
-          data.validate_in_and_out_domain=$validate_in_and_out_domain \
-          wandb.sweep.enabled=true \
-          data.train_batch_size=32 \
-          data.val_batch_size=32 \
-          data.test_batch_size=32 \
-          $config
-        sleep 61
-      done
+      echo "Submitting REARC ($sweep_type): $config"
+      sbatch run_experiment.submit \
+        $use_gen_test_set \
+        $validate_in_and_out_domain \
+        wandb.sweep.enabled=true \
+        data.train_batch_size=32 \
+        data.val_batch_size=32 \
+        data.test_batch_size=32 \
+        $config
+      sleep 61
     done
   done
 fi
