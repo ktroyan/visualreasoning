24bc889ef90b988c1a488dd2a20010990504ca72
ktroyan <klim.troyan@gmail.com>
2025-05-05 18:18:59 +0200

    handle checkpointing monitored metric for OOD validation

--- training.py
+++ training.py
@@ -266,9 +266,9 @@ def init_callbacks(config, training_folder):
 
     # Model checkpoint callback
     if config.training.checkpointing.enabled:
-        if config.training.checkpointing.monitored_metric == 'val_loss':
+        if "loss" in config.training.checkpointing.monitored_metric:
             mode = 'min'
-        elif config.training.checkpointing.monitored_metric == 'val_acc':
+        elif "acc" in config.training.checkpointing.monitored_metric:
             mode = 'max'
         else:
             raise ValueError(f"Unknown monitored metric for model checkpoint: {config.training.checkpointing.monitored_metric}")
@@ -285,9 +285,9 @@ def init_callbacks(config, training_folder):
 
     # Early stopping callback
     if config.training.early_stopping.enabled:
-        if config.training.early_stopping.monitored_metric == 'val_loss':
+        if "loss" in config.training.early_stopping.monitored_metric:   # handle the case where we have "val_loss" or "gen_val_loss"
             mode = 'min'
-        elif config.training.early_stopping.monitored_metric == 'val_acc':
+        elif "acc" in config.training.early_stopping.monitored_metric:  # handle the case where we have "val_acc" or "gen_val_acc"
             mode = 'max'
         else:
             raise ValueError(f"Unknown monitored metric for early stopping: {config.training.early_stopping.monitored_metric}")
