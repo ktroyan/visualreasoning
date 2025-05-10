97a1d36e9d1002415cb34d614fe3cfead6d05a57
Pascal Sager <paes.sager@gmail.com>
2025-05-10 16:48:39 +0200

    Redo failed runs

--- slurm/failed_runs.sh
+++ slurm/failed_runs.sh
@@ -4,7 +4,7 @@
 configs=(
   "BEFOREARC compositionality exp_setting_1 experiment_5"
   "BEFOREARC compositionality exp_setting_2 experiment_5"
-  "BEFOREARC compositionality exp_setting_2 experiment_4"
+  # "BEFOREARC compositionality exp_setting_2 experiment_4"
   # "BEFOREARC compositionality exp_setting_1 experiment_2"
   # "BEFOREARC compositionality exp_setting_2 experiment_2"
   # "BEFOREARC compositionality exp_setting_3 experiment_3"
