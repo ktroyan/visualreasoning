f11bfd51b66961da4d4eb0bebf27225c6de357f4
Pascal Sager <paes.sager@gmail.com>
2025-05-22 08:08:16 +0200

    Update failed runs (seed)

--- slurm/failed_runs.sh
+++ slurm/failed_runs.sh
@@ -2,42 +2,56 @@
 
 # List of experiment configurations
 configs=(
-  "BEFOREARC compositionality exp_setting_1 experiment_2"
-  "BEFOREARC compositionality exp_setting_1 experiment_5"
-  "BEFOREARC compositionality exp_setting_2 experiment_2"
-  "BEFOREARC compositionality exp_setting_2 experiment_3"
-  "BEFOREARC compositionality exp_setting_2 experiment_4"
-  "BEFOREARC compositionality exp_setting_2 experiment_5"
-  "BEFOREARC compositionality exp_setting_3 experiment_2"
-  "BEFOREARC compositionality exp_setting_3 experiment_3"
-  "BEFOREARC compositionality exp_setting_3 experiment_4"
-  "BEFOREARC compositionality exp_setting_3 experiment_5"
-  "BEFOREARC sys-gen exp_setting_1 experiment_3"
-  "BEFOREARC sys-gen exp_setting_1 experiment_4"
-  "BEFOREARC sys-gen exp_setting_1 experiment_5"
-  "BEFOREARC sys-gen exp_setting_2 experiment_4"
-  "BEFOREARC sys-gen exp_setting_2 experiment_2"
-  "BEFOREARC sys-gen exp_setting_3 experiment_3"
-  "BEFOREARC sys-gen exp_setting_3 experiment_4"
-  "BEFOREARC sys-gen exp_setting_3 experiment_5"
-  "BEFOREARC sys-gen exp_setting_3 experiment_5"
-  "BEFOREARC sys-gen exp_setting_4 experiment_3"
-  "BEFOREARC sys-gen exp_setting_4 experiment_5"
-  "BEFOREARC sys-gen exp_setting_5 experiment_3"
-  "BEFOREARC sys-gen exp_setting_5 experiment_4"
-  "BEFOREARC sys-gen exp_setting_5 experiment_5"
+  "BEFOREARC compositionality exp_setting_1 experiment_2 2025"
+  "BEFOREARC compositionality exp_setting_1 experiment_2 4269"
+  "BEFOREARC compositionality exp_setting_1 experiment_4 2025"
+  "BEFOREARC compositionality exp_setting_1 experiment_5 4269"
+  "BEFOREARC compositionality exp_setting_2 experiment_2 2025"
+  "BEFOREARC compositionality exp_setting_2 experiment_2 4269"
+  "BEFOREARC compositionality exp_setting_2 experiment_3 2025"
+  "BEFOREARC compositionality exp_setting_2 experiment_4 2025"
+  "BEFOREARC compositionality exp_setting_2 experiment_5 4269"
+  "BEFOREARC compositionality exp_setting_2 experiment_5 2025"
+  "BEFOREARC compositionality exp_setting_3 experiment_2 2025"
+  "BEFOREARC compositionality exp_setting_3 experiment_2 4269"
+  "BEFOREARC compositionality exp_setting_3 experiment_4 2025"
+  "BEFOREARC compositionality exp_setting_3 experiment_5 4269"
+  "BEFOREARC compositionality exp_setting_3 experiment_5 2025"
+  "BEFOREARC sys-gen exp_setting_1 experiment_3 2025"
+  "BEFOREARC sys-gen exp_setting_1 experiment_4 4269"
+  "BEFOREARC sys-gen exp_setting_1 experiment_5 2025"
+  "BEFOREARC sys-gen exp_setting_2 experiment_2 2025"
+  "BEFOREARC sys-gen exp_setting_2 experiment_4 4269"
+  "BEFOREARC sys-gen exp_setting_2 experiment_4 2025"
+  "BEFOREARC sys-gen exp_setting_2 experiment_5 2025"
+  "BEFOREARC sys-gen exp_setting_3 experiment_3 2025"
+  "BEFOREARC sys-gen exp_setting_3 experiment_3 4269"
+  "BEFOREARC sys-gen exp_setting_3 experiment_4 2025"
+  "BEFOREARC sys-gen exp_setting_3 experiment_4 4269"
+  "BEFOREARC sys-gen exp_setting_3 experiment_5 2025"
+  "BEFOREARC sys-gen exp_setting_3 experiment_5 4269"
+  "BEFOREARC sys-gen exp_setting_4 experiment_3 2025"
+  "BEFOREARC sys-gen exp_setting_4 experiment_3 4269"
+  "BEFOREARC sys-gen exp_setting_4 experiment_4 2025"
+  "BEFOREARC sys-gen exp_setting_4 experiment_4 4269"
+  "BEFOREARC sys-gen exp_setting_4 experiment_5 2025"
+  "BEFOREARC sys-gen exp_setting_4 experiment_5 4269"
+  "BEFOREARC sys-gen exp_setting_5 experiment_3 2025"
+  "BEFOREARC sys-gen exp_setting_5 experiment_4 4269"
+  "BEFOREARC sys-gen exp_setting_5 experiment_4 2025"
 )
 
 # Iterate over each configuration and submit the job
 for config in "${configs[@]}"; do
-  read -r data_env study setting name <<< "$config"
+  read -r data_env study setting name seed <<< "$config"
 
-  sbatch run_experiment.submit \
+  echo sbatch run_experiment.submit \
     wandb.sweep.enabled=false \
     wandb.wandb_entity_name=VisReas-ETHZ \
     wandb.wandb_project_name=VisReas-project-${data_env}-llada-final \
     base.data_env=${data_env} \
     experiment.study=${study} \
     experiment.setting=${setting} \
-    experiment.name=${name}
+    experiment.name=${name} \
+    base.seed=${seed}
 done
